660023102da95f82e2a49a33fb1f4ac1454b77ce
TheShyDev-Yoogo <yoogo.production@gmail.com>
2023-09-30 15:50:41 -0400

    update architecture of homepage and adding ABout us page

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,18 +1,16 @@
-import Hero from '@/components/hero'
-import Features from '@/components/features'
-import Newsletter from '@/components/newsletter'
-import PartnerZigzag from '@/components/partners-zigzag'
-import DescriptionZigZag from '@/components/description-zigzag'
-import Introduction from '@/components/intro'
+import DescriptionZigZag from '@/app/(default)/components/description-zigzag'
 import flavicon from 'public/favicon.ico'
+import Hero from './components/hero'
+import Features from './components/features'
+import Newsletter from './components/newsletter'
+import PartnerZigzag from './components/partners-zigzag'
+import Introduction from './components/intro'
 
 export const metadata = {
   title: 'Dionysas',
   description: 'Revolutionize your restaurant with AI',
   icons: flavicon.src 
 };
-
-
 
 
 export default function Home() {
---
+++ app/aboutus/page.tsx
@@ -0,0 +1,28 @@
+export const metadata = {
+    title: 'About us',
+    description: 'About Dionysas',
+  }
+  
+  import Link from 'next/link'
+
+  export default function AboutUs() {
+    return (
+      <section className="bg-gradient-to-b from-gray-100 to-white">
+        <div className="max-w-6xl mx-auto px-4 sm:px-6">
+          <div className="pt-32 pb-12 md:pt-40 md:pb-20">
+  
+            {/* Page header */}
+            <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
+              <h1 className="h1">About us</h1>
+            </div>
+
+            <div className="max-w-sm mx-auto">
+              <h1 className="h2">hello</h1>
+            </div>
+  
+          </div>
+        </div>
+      </section>
+    )
+  }
+  
